64a16936c17f9402a46315de049492b5a416d8ec
Håkon Wiik Ånes <hwaanes@gmail.com>
2020-10-13 14:35:44 +0200

    Add data directory, fix Travis pip install and remove 3.9
    Signed-off-by: Håkon Wiik Ånes <hwaanes@gmail.com>

--- test_notebooks.sh
+++ test_notebooks.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Notes:
+# * Visual Studio Code doesn't write some symbols to the .ipynb file the same way that Jupyter Notebook does, making some
+#   cell output comparisons fail.
+
 # Add number prefix to this list if the notebook is to be tested with
 # $ pytest --nbval notebook.ipynb
 declare -a NOTEBOOKS=("04")
@@ -10,7 +14,6 @@ DIR=${PWD}/
 FILE_PATTERN="*.ipynb"
 FILES=($DIR$FILE_PATTERN)
 for FILE in "${FILES[@]}"; do
-    echo ${FILE}
     NUMBER="${FILE%-*}"
     NUMBER="${NUMBER:(-3)}"
     if [[ " ${NOTEBOOKS[@]} " == *${NUMBER}* ]]; then
